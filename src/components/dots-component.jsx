59c50333a61799e9fd2158d0d38e8ec6f6c4ada2
Luka-dot <79lukask@gmail.com>
2020-12-02 12:01:12 -0500

    Dots animation for expand and shrink on click

--- src/components/dots-component.jsx
+++ src/components/dots-component.jsx
@@ -1,22 +1,35 @@
-import React, {useRef, useEffect} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 
 import {TweenMax, Power3} from 'gsap';
 
 import './dots.styles.css';
 
 function Dots() {
+
     let circleContainer = useRef(null);
     let circle = useRef(null);
     let circleRed = useRef(null);
     let circleBlue = useRef(null);
 
+    const [circleStatus, setCircleStatus] = useState(false);
+
     useEffect(() => {
-        // TweenMax.to(circleContainer, 0, {css: {visibility: 'visible'}});
+        TweenMax.to(circleContainer, 0, {css: {visibility: 'visible'}});
         // TweenMax.from(circle, .8, {opacity: 0, x: 40, ease: Power3.easeOut});
         // TweenMax.from(circleRed, .8, {opacity: 0, x: 40, ease: Power3.easeOut, delay: .2});
         // TweenMax.from(circleBlue, .8, {opacity: 0, x: 40, ease: Power3.easeOut, delay: .4})
         TweenMax.staggerFrom([circle, circleRed, circleBlue], .8, {opacity: 0, x: 40, ease: Power3.easeOut}, .3)
     }, []);
+
+    const handleExpand = () => {
+        TweenMax.to(circleRed, .8, {width: 200, height: 200, ease: Power3.easeOut})
+        setCircleStatus(true)
+    };
+
+    const handleShrink = () => {
+        TweenMax.to(circleRed, .8, {width: 75, height: 75, ease: Power3.easeOut})
+        setCircleStatus(false)
+    }
 
     return (
         <div className='circle-container'
@@ -26,6 +39,7 @@ function Dots() {
                 ref={el => circle = el }
             ></div>
             <div className='circle red'
+            onClick={circleStatus !== true ? handleExpand : handleShrink}
                 ref={el => circleRed = el }
             ></div>
             <div className='circle blue'
