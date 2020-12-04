39b80af90019b262d2f344788054af4c5040786f
Luka-dot <79lukask@gmail.com>
2020-12-04 11:30:02 -0500

    image reveal finished

--- src/App.js
+++ src/App.js
@@ -47,17 +47,9 @@ function App() {
         >
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
       <ImageTransition />
-      {/* <Dots /> */}
+      <Dots />
     </div>
   );
 }
