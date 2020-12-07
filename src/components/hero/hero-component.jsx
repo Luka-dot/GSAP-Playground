99961e02838f2587a2eb70af5019fb36b1007ff5
Luka-dot <79lukask@gmail.com>
2020-12-07 15:53:53 -0500

    animation for Hero images completed

--- src/components/hero/hero-component.jsx
+++ src/components/hero/hero-component.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import { TweenLite, Power3} from 'gsap';
+import React, {useRef, useEffect} from 'react';
+import { TweenMax, TimelineLite, Power3} from 'gsap';
 
 import './hero.styles.scss';
-import hero1Picture from '../../assets/hero1.jpg';
-import hero2Picture from '../../assets/hero2.jpg';
+import hero1Picture from '../../assets/hero2.jpg';
+import hero2Picture from '../../assets/hero1.jpg';
 import rightArr from '../../assets/right-arrow.svg';
 
 function Hero() {
+    let hero = useRef(null);
+    let images = useRef(null);
+    let conten = useRef(null);
+
+    useEffect(() => {
+        const imageOne = images.firstElementChild;
+        const imageTwo = images.lastElementChild;
+
+        let tl = new TimelineLite();
+        // images animations
+        TweenMax.to(hero, 0, {css: {visibility: 'visible'}});  // this avoids flashing on load.
+        tl.from(imageOne, 1.8, {y: 1380, ease: Power3.easeOut})
+            .from(imageOne.firstElementChild, 2, {scale: 1.6, ease: Power3.easeOut}, .2)
+            .from(imageTwo, 1.5, {y: 800, ease: Power3.easeOut}, .5)
+            .from(imageTwo.firstElementChild, 1.6, {scale: 1.6, ease: Power3.easeOut}, 1)
+    })
 
     return(
-        <div className="hero">
+        <div className="hero" ref={el => hero = el}>
             <div className="hero-container">
                 <div className="hero-inner">
                     <div className="hero-content">                      
-                        <div className="hero-content-inner">
+                        <div className="hero-content-inner" ref={el => conten = el}>
                             <h1>
                                 <div className="hero-content-line">
                                     <div className="hero-content-line-inner">First line</div>
@@ -38,12 +54,12 @@ function Hero() {
                             </div>                       
                         </div>
                     <div className="hero-images">
-                        <div className="hero-images-inner">
+                        <div className="hero-images-inner" ref={el => images = el}>
                             <div className="hero-image one">
                                 <img src={hero1Picture} alt="one" />
                             </div>
                             <div className="hero-image two">
-                                <img src={hero2Picture} alt="two" />
+                                <img className="image-two" src={hero2Picture} alt="two" />
                             </div>
                         </div>
                     </div>   
